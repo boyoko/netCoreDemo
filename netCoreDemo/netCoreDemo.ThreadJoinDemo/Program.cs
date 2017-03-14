21c8e0b754bc3c320983e57db3b740f283b9a50a
boyoko <zhaods88@163.com>
2017-03-14 14:00:22 +0800

    从github上clone 空项目，然后新建解决方案

---
+++ netCoreDemo/netCoreDemo.ThreadJoinDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netCoreDemo.ThreadJoinDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
