9e8e9eff31e5827e37c90dde888b1db1d9d3adeb
boyoko <zhaods88@163.com>
2017-03-15 10:15:19 +0800

    添加host.json 文件，

--- netCoreDemo/Angular2.Api/Program.cs
+++ netCoreDemo/Angular2.Api/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Angular2.Api
 {
@@ -12,9 +13,15 @@ namespace Angular2.Api
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("hosting.json", optional: true)
+                            .Build();
+
+
             var host = new WebHostBuilder()
                 .UseKestrel()
-                //.UseUrls("http://192.168.1.113:5000")
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
